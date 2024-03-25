2634b7cd65fc3adf395de904d0a6cbbca7d99d03
Valentin0804 <valenmoli_18@hotmail.com>
2024-03-25 19:24:19 -0300

    resolucion ejercicio_01.py

--- Practica_01/ejercicio_01.py
+++ Practica_01/ejercicio_01.py
@@ -1,4 +1,5 @@
 def maximo_basico(a:float, b:float) -> float:
+    #Utilizando if
     if a > b:
         return a
     else:
@@ -6,3 +7,17 @@ def maximo_basico(a:float, b:float) -> float:
 
 assert maximo_basico(10, 5) == 10
 assert maximo_basico(9, 18) == 18
+
+def maximo_libreria(a: float, b:float) -> float:
+    #Utilizando max
+    return max(a,b)
+
+assert maximo_libreria(10, 5) == 10
+assert maximo_libreria(9, 18) == 18
+
+def maximo_ternario(a: float, b: float) -> float:
+    #Utilizando el operador ternario
+    return a if a > b else b
+
+assert maximo_ternario(10, 5) == 10
+assert maximo_ternario(9, 18) == 18
